419b1453082b1b0d38d6cbe65fb0696ab476f21e
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2024-09-03 16:04:51 +0200

    CI/CD fix

--- Frontend/src/app/(DashboardLayout)/dashboard/organization/page.tsx
+++ Frontend/src/app/(DashboardLayout)/dashboard/organization/page.tsx
@@ -17,7 +17,7 @@ const OrganizationPage = () => {
             <li>E-mail: {user?.email}</li>
           </ul>
           <Typography>
-            <b>Description:</b> Information about this person's organization. Invite colleagues, maybe some rolebased stuff, etc.
+            <b>Description:</b> Information about this person&apos;s organization. Invite colleagues, maybe some rolebased stuff, etc.
           </Typography>
         </>
       </DashboardCard>
