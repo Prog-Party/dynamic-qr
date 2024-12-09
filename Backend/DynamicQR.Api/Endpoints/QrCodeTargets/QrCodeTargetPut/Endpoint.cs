2df167109a0069c866a041b2a40891c042050132
Jens den Braber <jens.denbraber@gmail.com>
2024-12-09 17:03:51 +0100

    endpoint test

--- Backend/DynamicQR.Api/Endpoints/QrCodeTargets/QrCodeTargetPut/Endpoint.cs
+++ Backend/DynamicQR.Api/Endpoints/QrCodeTargets/QrCodeTargetPut/Endpoint.cs
@@ -1,51 +1,51 @@
-﻿using DynamicQR.Api.Attributes;
-using DynamicQR.Api.Mappers;
-using MediatR;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Moq;
-using System.Net;
+﻿//using DynamicQR.Api.Attributes;
+//using DynamicQR.Api.Mappers;
+//using MediatR;
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.Azure.Functions.Worker.Http;
+//using Microsoft.Azure.Storage;
+//using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.OpenApi.Models;
+//using Moq;
+//using System.Net;
 
-namespace DynamicQR.Api.Endpoints.QrCodeTargets.QrCodeTargetPut;
+//namespace DynamicQR.Api.Endpoints.QrCodeTargets.QrCodeTargetPut;
 
-public sealed class QrCodeTargetPut : EndpointsBase
-{
-    public QrCodeTargetPut(IMediator mediator, ILoggerFactory loggerFactory) :
-        base(mediator, loggerFactory.CreateLogger<QrCodeTargetPut>())
-    { }
+//public sealed class QrCodeTargetPut : EndpointsBase
+//{
+//    public QrCodeTargetPut(IMediator mediator, ILoggerFactory loggerFactory) :
+//        base(mediator, loggerFactory.CreateLogger<QrCodeTargetPut>())
+//    { }
 
-    [Function(nameof(QrCodeTargetPut))]
-    [OpenApiOperation(nameof(QrCodeTargetPut), Tags.QrCodeTarget,
-       Summary = "Update a certain qr code target.")
-    ]
-    [OpenApiPathIdentifier]
-    [OpenApiJsonPayload(typeof(Request))]
-    [OpenApiJsonResponse(typeof(Response), Description = "Update a certain qr code target")]
-    [OpenApiResponseWithoutBody(HttpStatusCode.BadGateway, Description = "No qr code target found with the given identifier.")]
-    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "put", Route = "qr-code-targets/{id}")] HttpRequestData req, string id)
-    {
-        var request = await ParseBody<Request>(req);
-        if (request.Error != null) return request.Error;
+//    [Function(nameof(QrCodeTargetPut))]
+//    [OpenApiOperation(nameof(QrCodeTargetPut), Tags.QrCodeTarget,
+//       Summary = "Update a certain qr code target.")
+//    ]
+//    [OpenApiPathIdentifier]
+//    [OpenApiJsonPayload(typeof(Request))]
+//    [OpenApiJsonResponse(typeof(Response), Description = "Update a certain qr code target")]
+//    [OpenApiResponseWithoutBody(HttpStatusCode.BadGateway, Description = "No qr code target found with the given identifier.")]
+//    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "put", Route = "qr-code-targets/{id}")] HttpRequestData req, string id)
+//    {
+//        var request = await ParseBody<Request>(req);
+//        if (request.Error != null) return request.Error;
 
-        Application.QrCodes.Commands.UpdateQrCodeTarget.Command? coreCommand = request.Result.ToCore(id);
+//        Application.QrCodes.Commands.UpdateQrCodeTarget.Command? coreCommand = request.Result.ToCore(id);
 
-        Application.QrCodes.Commands.UpdateQrCodeTarget.Response coreResponse;
+//        Application.QrCodes.Commands.UpdateQrCodeTarget.Response coreResponse;
 
-        try
-        {
-            coreResponse = await _mediator.Send(coreCommand!, It.IsAny<CancellationToken>());
-        }
-        catch (StorageException)
-        {
-            return req.CreateResponse(HttpStatusCode.BadGateway);
-        }
+//        try
+//        {
+//            coreResponse = await _mediator.Send(coreCommand!, It.IsAny<CancellationToken>());
+//        }
+//        catch (StorageException)
+//        {
+//            return req.CreateResponse(HttpStatusCode.BadGateway);
+//        }
 
-        Response? responseContent = coreResponse.ToContract();
+//        Response? responseContent = coreResponse.ToContract();
 
-        return await CreateJsonResponse(req, responseContent, HttpStatusCode.OK);
-    }
-}
+//        return await CreateJsonResponse(req, responseContent, HttpStatusCode.OK);
+//    }
+//}
--- Backend/DynamicQR.Api/Endpoints/QrCodes/QrCodePut/Endpoint.cs
+++ Backend/DynamicQR.Api/Endpoints/QrCodes/QrCodePut/Endpoint.cs
@@ -1,55 +1,55 @@
-﻿using DynamicQR.Api.Attributes;
-using DynamicQR.Api.Extensions;
-using DynamicQR.Api.Mappers;
-using MediatR;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Moq;
-using System.Net;
+﻿//using DynamicQR.Api.Attributes;
+//using DynamicQR.Api.Extensions;
+//using DynamicQR.Api.Mappers;
+//using MediatR;
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.Azure.Functions.Worker.Http;
+//using Microsoft.Azure.Storage;
+//using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.OpenApi.Models;
+//using Moq;
+//using System.Net;
 
-namespace DynamicQR.Api.Endpoints.QrCodes.QrCodePut;
+//namespace DynamicQR.Api.Endpoints.QrCodes.QrCodePut;
 
-public sealed class QrCodePut : EndpointsBase
-{
-    public QrCodePut(IMediator mediator, ILoggerFactory loggerFactory) :
-        base(mediator, loggerFactory.CreateLogger<QrCodePut>())
-    { }
+//public sealed class QrCodePut : EndpointsBase
+//{
+//    public QrCodePut(IMediator mediator, ILoggerFactory loggerFactory) :
+//        base(mediator, loggerFactory.CreateLogger<QrCodePut>())
+//    { }
 
-    [Function(nameof(QrCodePut))]
-    [OpenApiOperation(nameof(QrCodePut), Tags.QrCode,
-       Summary = "Update a certain qr code.")
-    ]
-    [OpenApiHeaderOrganizationIdentifier]
-    [OpenApiPathIdentifier]
-    [OpenApiJsonPayload(typeof(Request))]
-    [OpenApiJsonResponse(typeof(Response), Description = "Update a certain qr code")]
-    [OpenApiResponseWithoutBody(HttpStatusCode.BadRequest, Description = "No qr code found with the given identifier.")]
-    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "put", Route = "qr-codes/{id}")] HttpRequestData req, string id)
-    {
-        string organizationId = req.GetHeaderAttribute<OpenApiHeaderOrganizationIdentifierAttribute>();
+//    [Function(nameof(QrCodePut))]
+//    [OpenApiOperation(nameof(QrCodePut), Tags.QrCode,
+//       Summary = "Update a certain qr code.")
+//    ]
+//    [OpenApiHeaderOrganizationIdentifier]
+//    [OpenApiPathIdentifier]
+//    [OpenApiJsonPayload(typeof(Request))]
+//    [OpenApiJsonResponse(typeof(Response), Description = "Update a certain qr code")]
+//    [OpenApiResponseWithoutBody(HttpStatusCode.BadRequest, Description = "No qr code found with the given identifier.")]
+//    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "put", Route = "qr-codes/{id}")] HttpRequestData req, string id)
+//    {
+//        string organizationId = req.GetHeaderAttribute<OpenApiHeaderOrganizationIdentifierAttribute>();
 
-        var request = await ParseBody<Request>(req);
-        if (request.Error != null) return request.Error;
+//        var request = await ParseBody<Request>(req);
+//        if (request.Error != null) return request.Error;
 
-        Application.QrCodes.Commands.UpdateQrCode.Command? coreCommand = QrCodesMappers.ToCore(request.Result, id, organizationId);
+//        Application.QrCodes.Commands.UpdateQrCode.Command? coreCommand = QrCodesMappers.ToCore(request.Result, id, organizationId);
 
-        Application.QrCodes.Commands.UpdateQrCode.Response coreResponse;
+//        Application.QrCodes.Commands.UpdateQrCode.Response coreResponse;
 
-        try
-        {
-            coreResponse = await _mediator.Send(coreCommand!, It.IsAny<CancellationToken>());
-        }
-        catch (StorageException)
-        {
-            return await CreateJsonResponse(req, null, HttpStatusCode.BadGateway);
-        }
+//        try
+//        {
+//            coreResponse = await _mediator.Send(coreCommand!, It.IsAny<CancellationToken>());
+//        }
+//        catch (StorageException)
+//        {
+//            return await CreateJsonResponse(req, null, HttpStatusCode.BadGateway);
+//        }
 
-        Response? responseContent = coreResponse.ToContract();
+//        Response? responseContent = coreResponse.ToContract();
 
-        return await CreateJsonResponse(req, responseContent, HttpStatusCode.OK);
-    }
-}
+//        return await CreateJsonResponse(req, responseContent, HttpStatusCode.OK);
+//    }
+//}
