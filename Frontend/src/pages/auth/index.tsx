cf810053fa44fafd062551cc822e2ce900710571
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2024-12-12 22:38:14 +0100

    Add pages and styles

--- Frontend/src/pages/auth/index.tsx
+++ Frontend/src/pages/auth/index.tsx
@@ -1,20 +1,20 @@
 'use client'
 import PageContainer from '@/components/container/PageContainer';
+import { Box, Grid, Typography } from '@mui/material';
+// components
 import GenerateStaticQr from '@/components/generate/GenerateStaticQR';
 import DashboardCard from '@/components/shared/DashboardCard';
-import { Box, Grid, Typography } from '@mui/material';
 
 const Dashboard = () => {
-
 
   return (
     <PageContainer title="Dashboard" description="this is Dashboard">
       <Box>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={12}>
-            <DashboardCard title="Goals">
+            <DashboardCard title="DASHBOARD!!! When logged in">
               <Typography>
-                <b>Title:</b> Homes
+                <b>Title:</b> Home
                 <br />
                 <b>Description:</b> The central hub for your website. Introduces the service with a compelling headline, a brief overview of dynamic QR codes, and a clear call-to-action (CTA) button that leads to the signup or demo page.
                 <br />
