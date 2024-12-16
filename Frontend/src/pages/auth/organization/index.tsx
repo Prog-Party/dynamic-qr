0387ab2c709a69e326dfa21c3e3b61b025be3a94
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2024-12-16 22:57:26 +0100

    Add API and callers

--- Frontend/src/pages/auth/organization/index.tsx
+++ Frontend/src/pages/auth/organization/index.tsx
@@ -1,12 +1,30 @@
 "use client"
+import { createCode } from "@/api/qr-codes"
 import PageContainer from "@/components/container/PageContainer"
 import DashboardCard from "@/components/shared/DashboardCard"
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
-import { Typography } from "@mui/material"
+import { Button, Typography } from "@mui/material"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
 const OrganizationPage = () => {
 
   const { user } = useAuth0()
+  const [qrCodes, setQrCodes] = useState()
+
+  const createQrCode = async () => {
+    const result = await createCode(user!.organizationId, "temp")
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // const apiResult = await getCodes(user!.organizationId)
+      // setQrCodes(apiResult)
+      // console.log(apiResult)
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <PageContainer title="Organization" description="this is Organization page">
@@ -20,7 +38,20 @@ const OrganizationPage = () => {
           <Typography>
             <b>Description:</b> Information about this person&apos;s organization. Invite colleagues, maybe some rolebased stuff, etc.
           </Typography>
+          <p>
+            QR Codes: {qrCodes}
+          </p>
         </>
+      </DashboardCard>
+      <DashboardCard title="Overview QR Codes">
+        <Link href="/auth/qr-codes/c57fc106-b7f9-4f48-b70c-f0ea1fe39752">
+          QR Code 1 (id c57fc106-b7f9-4f48-b70c-f0ea1fe39752)
+        </Link>
+      </DashboardCard>
+      <DashboardCard title="Create QR Code">
+        <Button variant="contained" color="primary" onClick={createQrCode}>
+          Create QR Code
+        </Button>
       </DashboardCard>
     </PageContainer>
   )
